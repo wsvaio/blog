881763c592ab01b0f07b54b3f2137c66afeb7160
wsvaio <wsvaio@qq.com>
2023-11-14 15:13:56 +0800

    sync

--- .output/server/chunks/app/_nuxt/entry-styles.d582e2c2.mjs
+++
@@ -1,8 +0,0 @@
-const main = "";
-
-const __uno = "#--unocss--{layer:__ALL__}";
-
-const entryStyles_d582e2c2 = [main, __uno];
-
-export { entryStyles_d582e2c2 as default };
-//# sourceMappingURL=entry-styles.d582e2c2.mjs.map
--- .output/server/chunks/index.mjs
+++
@@ -1,6 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-
-const db = new PrismaClient();
-
-export { db as d };
-//# sourceMappingURL=index.mjs.map
