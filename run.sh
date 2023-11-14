ef0d3bc0a2133046e16984beca6ab992dfe83393
wsvaio <wsvaio@qq.com>
2023-11-14 12:24:37 +0800

    sync

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ docker rm -fv $name
 
 docker run -d \
   -v /app/service/blog:/app \
-  -e PORT=7100
+  -e PORT=7100 \
   -p 7100:7100 \
   --name $name \
   oven/bun bun /app/.output/server/index.mjs
