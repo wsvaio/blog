e1157a7549c2baa9c9c73e67a697827bc013f84e
wsvaio <wsvaio@qq.com>
2023-11-16 15:27:44 +0800

    sync

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ docker run \
   -d \
   -v /app/service/blog:/app \
   -e PORT=7100 \
-  -e DATABASE_URL=$database_url
+  -e DATABASE_URL=$database_url \
   -p 7100:7100 \
   --name $name \
   node node /app/.output/server/index.mjs
