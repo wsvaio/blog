89d07b72b88152b6cdf42f9930495f127c90f6ff
wsvaio <wsvaio@qq.com>
2023-11-17 13:13:58 +0800

    sync

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ docker run \
   -d \
   -v /app/service/blog:/app \
   -e PORT=7100 \
-  -e DATABASE_URL=$database_url \
+  -e DATABASE_URL=postgresql://postgres:147wsvaio.,,@121.40.126.12:5432/newblog?schema=public \
   -p 7100:7100 \
   --name $name \
   node node /app/.output/server/index.mjs
