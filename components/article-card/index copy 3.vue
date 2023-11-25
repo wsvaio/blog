ab7d10f7efa4e6b38af11a9ae55dbf5fc4f0498a
wsvaio <wsvaio@qq.com>
2023-11-25 22:10:23 +0800

    feat: 首页的文章卡片

---
+++ components/article-card/index copy 3.vue
@@ -0,0 +1,92 @@
+<script setup lang="ts">
+import { marked } from "marked";
+
+const { data = {} } = defineProps<{
+	data: Record<any, any>;
+}>();
+const isMounted = $(useMounted());
+
+let images = $ref<string[]>([]);
+let textContent = $ref("");
+
+watchEffect(() => {
+	if (!isMounted) return;
+	const domparser = new DOMParser();
+	const doc = domparser.parseFromString(marked(data.content), "text/html");
+	images = [...doc.querySelectorAll("img")].map(item => item.src);
+	textContent = [...doc.querySelectorAll("*")]
+		.map(item => item.textContent)
+		.join(" ")
+		.trim();
+});
+</script>
+
+<template>
+	<div grid="~ cols-1fr" class="card" overflow="hidden" pos="relative">
+		<div
+			flex="~ col"
+			justify="end"
+			items="center"
+			color="[#fffffff2]"
+			p="1em"
+			class="box-border"
+			pos="relative"
+			:style="{
+				height: images.length ? '250px' : 'auto',
+			}"
+		>
+			<div
+				grid="~"
+				:style="{
+					gridTemplateColumns: `repeat(${images.length}, 1fr)`,
+				}"
+				pos="absolute"
+				inset="0"
+			>
+				<img
+					v-for="item in images"
+					w="full" object="cover" :src="item"
+					h="250px"
+				/>
+			</div>
+
+			<div pos="absolute" inset="0" bg="[rgba(0,0,0,0.16)]" />
+
+			<h2 mt="0" z="10">
+				<nuxt-link
+					:to="`/article/${data.id}`"
+					hover="text-[var(--secondary-color)]"
+					underline="transparent"
+					color="[#fffffff2]"
+				>
+					{{ data.title }}
+				</nuxt-link>
+			</h2>
+
+			<div
+				flex="~" items="center" gap=".5em" z="10"
+				text="14px"
+			>
+				<div>
+					<button class="i-material-symbols-edit-calendar-outline" text="16px" font="italic" />
+					<span>{{ new Date(data.updateAt).toLocaleString() }}</span>
+				</div>
+				<span>|</span>
+				<div>
+					<button class="i-material-symbols-edit-calendar-outline" text="16px" font="italic" />
+					<span>{{ data.type.name }}</span>
+				</div>
+				<span>|</span>
+				<div>
+					<span v-for="item in data.tags" font="italic">#{{ item.name }}</span>
+				</div>
+			</div>
+		</div>
+
+		<p v-if="textContent" m="0" p="2em" z="10">
+			{{ textContent.length > 256 ? `${textContent.slice(0, 128)}……` : textContent }}
+		</p>
+	</div>
+</template>
+
+<style lang="less" scoped></style>
--- components/article-card/index.vue
+++ components/article-card/index.vue
@@ -1,19 +1,29 @@
 <script setup lang="ts">
 import { marked } from "marked";
 
-const { data = {} } = defineProps<{
+const { data = {}, type = "left" } = defineProps<{
 	data: Record<any, any>;
+	type: "left" | "right" | "full";
 }>();
+
 const isMounted = $(useMounted());
 
-let images = $ref<string[]>([]);
+// let images = $ref<string[]>([]);
+// const image = $computed(() => {
+// 	if (images[0]) return images[0];
+// 	if (bgImage?.value?.imgurl) return bgImage?.value?.imgurl;
+// 	executeBgImage();
+// });
+
+let image = $ref("");
 let textContent = $ref("");
 
-watchEffect(() => {
+watchEffect(async () => {
 	if (!isMounted) return;
 	const domparser = new DOMParser();
 	const doc = domparser.parseFromString(marked(data.content), "text/html");
-	images = [...doc.querySelectorAll("img")].map(item => item.src);
+	// images = [...doc.querySelectorAll("img")].map(item => item.src);
+	image = [...doc.querySelectorAll("img")].find(item => item.src)?.src || (await $fetch<any>("/api/dmoe")).imgurl;
 	textContent = [...doc.querySelectorAll("*")]
 		.map(item => item.textContent)
 		.join(" ")
@@ -22,71 +32,135 @@ watchEffect(() => {
 </script>
 
 <template>
-	<div grid="~ cols-1fr" class="card" overflow="hidden" pos="relative">
+	<div
+		class="article-card"
+		grid="~"
+		pos="relative"
+		overflow="hidden"
+		bg="black"
+		rounded="1.5"
+		:class="[type]"
+	>
+		<img
+			class="bgimage" :src="image" pos="absolute" inset="0"
+			scale="[1.55]" object="cover" h="full"
+		/>
+
+		<div class="image">
+			<img
+				:src="image" object="cover" h="full" z="10"
+				aspect-ratio="square"
+			/>
+		</div>
 		<div
+			color="white" py="48px" px="32px" z="1"
 			flex="~ col"
-			justify="end"
-			items="center"
-			color="[#fffffff2]"
-			p="1em"
-			class="box-border"
-			pos="relative"
-			:style="{
-				height: images.length ? '250px' : 'auto',
-			}"
 		>
-			<div
-				grid="~"
-				:style="{
-					gridTemplateColumns: `repeat(${images.length}, 1fr)`,
-				}"
-				pos="absolute"
-				inset="0"
+			<ul
+				m="0" p="0" list="none" flex="~"
+				gap=".5em" text="14px"
 			>
-				<img
-					v-for="item in images"
-					w="full" object="cover" :src="item"
-					h="250px"
-				/>
-			</div>
+				<li flex="~" gap=".5em" items="center">
+					<small rounded="full" bg="[var(--primary-color)]" p=".12em">
+						<div class="i-ic:round-edit-calendar" />
+					</small>
+					<span>{{ new Date(data.updateAt).toLocaleString() }}</span>
+				</li>
 
-			<div pos="absolute" inset="0" bg="[rgba(0,0,0,0.16)]" />
+				<li flex="~" gap=".5em" items="center" ml="auto">
+					<small rounded="full" bg="[var(--error-color)]" p=".12em">
+						<div class="i-material-symbols:category-outline" />
+					</small>
+					<span>{{ data.type.name }}</span>
+				</li>
 
-			<h2 mt="0" z="10">
+				<li flex="~" gap=".5em" items="center">
+					<small rounded="full" bg="[var(--success-color)]" p=".12em">
+						<div class="i-pepicons-pop:label" />
+					</small>
+
+					<span>{{ data?.tags?.map((item:any) => item.name).join('·') }}</span>
+				</li>
+			</ul>
+
+			<h2 z="10" font="normal">
 				<nuxt-link
 					:to="`/article/${data.id}`"
 					hover="text-[var(--secondary-color)]"
 					underline="transparent"
-					color="[#fffffff2]"
+					color="inherit"
 				>
 					{{ data.title }}
 				</nuxt-link>
 			</h2>
 
-			<div
-				flex="~" items="center" gap=".5em" z="10"
-				text="14px"
-			>
-				<div>
-					<button class="i-material-symbols-edit-calendar-outline" text="16px" font="italic" />
-					<span>{{ new Date(data.updateAt).toLocaleString() }}</span>
-				</div>
-				<span>|</span>
-				<div>
-					<button class="i-material-symbols-edit-calendar-outline" text="16px" font="italic" />
-					<span>{{ data.type.name }}</span>
-				</div>
-				<span>|</span>
-				<div>
-					<span v-for="item in data.tags" font="italic">#{{ item.name }}</span>
-				</div>
-			</div>
-		</div>
+			<p v-if="textContent" indent="2em" lh="[1.5]" mt="0">
+				{{ textContent.length > 64 ? `${textContent.slice(0, 64)}……` : textContent }}
+			</p>
 
-		<p v-if="textContent" m="0" p="2em" z="10">
-			{{ textContent.length > 256 ? `${textContent.slice(0, 128)}……` : textContent }}
-		</p>
+			<ul
+				mt="auto"
+				m="0" p="0" list="none" flex="~"
+				gap=".5em" text="14px"
+			>
+				<li>
+					{{ new Date(data.createAt).toLocaleString() }}
+				</li>
+				<li>|</li>
+				<li>{{ data.reads }} 阅读</li>
+				<li>|</li>
+				<li>{{ data.comments?.length }} 评论</li>
+				<li>|</li>
+				<li>{{ data.likes }} 点赞</li>
+			</ul>
+		</div>
 	</div>
 </template>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.article-card {
+  @media (width >= 768px) {
+    & > .image > img {
+      transition: transform 0.3s cubic-bezier(0, 0, 0.2, 1);
+    }
+
+    & > .bgimage {
+      filter: blur(1.875rem) brightness(0.75);
+    }
+
+    &:hover > .image > img {
+      transform: scale(1.05) rotate(1deg);
+    }
+
+    &.left {
+      grid-template-columns: 1fr 2fr;
+
+      & > .image {
+        clip-path: polygon(0 0, 100% 0, 94% 100%, 0 100%);
+      }
+    }
+
+    &.right {
+      grid-template-columns: 2fr 1fr;
+
+      & > .image {
+        clip-path: polygon(6% 0, 100% 0, 100% 100%, 0 100%);
+        order: 10;
+      }
+    }
+  }
+
+  @media (width < 768px) {
+    grid-template-columns: 1fr;
+
+    & > .image {
+      display: none;
+    }
+
+    & > .bgimage {
+      transform: scale(1);
+      filter: none;
+    }
+  }
+}
+</style>
