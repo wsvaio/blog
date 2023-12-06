889f3aac3806f4a454d71e70ac4a337649b3db29
wsvaio <wsvaio@qq.com>
2023-12-06 15:47:47 +0800

    feat: 添加微信api公众号接入测试

---
+++ server/api/official-accounts/index.ts
@@ -0,0 +1,25 @@
+import { createHash } from "node:crypto";
+
+export default eventHandler(event => {
+	const { signature, timestamp, nonce } = getQuery<{
+		signature: string;
+		timestamp: string;
+		nonce: number;
+		echostr: string;
+	}>(event);
+
+	const token = "wsvaio";
+
+	const tmpArr = [token, timestamp, nonce].sort();
+
+	const tmpStr = tmpArr.join("");
+
+	const sha1 = createHash("sha1");
+	sha1.update(tmpStr);
+
+	if (sha1.digest("hex") === signature)
+		return true;
+
+	else
+		return false;
+});
