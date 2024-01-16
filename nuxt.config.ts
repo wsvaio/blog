2f4124c8f44ced22ea6637055bf5c9132dbec958
wsvaio <wsvaio@qq.com>
2024-01-16 16:14:08 +0800

    fix: Cannot read properties of null (reading 'parentNode')Cannot read properties of null (reading 'parentNode')

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,74 +2,74 @@
 import ReactivityTransform from "@vue-macros/reactivity-transform/vite";
 
 export default defineNuxtConfig({
-	devtools: { enabled: true },
+  devtools: { enabled: true },
 
-	runtimeConfig: {},
+  runtimeConfig: {},
 
-	appConfig: {},
+  appConfig: {},
 
-	vite: {
-		vue: {},
-		plugins: [ReactivityTransform()],
-	},
+  vite: {
+    vue: {},
+    plugins: [ReactivityTransform()],
+  },
 
-	vue: {
-		defineModel: true,
-		propsDestructure: true,
+  vue: {
+    defineModel: true,
+    propsDestructure: true,
 
-	},
+  },
 
-	app: {
-		head: {
-			title: "WSの小屋",
-			meta: [
-				{ name: "author", content: "wsvaio@qq.com" },
-				{
-					name: "description",
-					content: "你就像天外来物一样求之不得！",
-				},
-			],
-			link: [
-				{
-					rel: "stylesheet",
-					type: "text/css",
-					href: "https://cdnjs.cloudflare.com/ajax/libs/aplayer/1.10.1/APlayer.min.css",
-				},
-			],
-			script: [
-				// { src: "https://fastly.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/autoload.js", type: "text/javascript" },
-				{ src: "https://apii.ctose.cn/live2d/Source-One/default/autoload.js", type: "text/javascript" },
-				{ src: "https://cdnjs.cloudflare.com/ajax/libs/aplayer/1.10.1/APlayer.min.js", type: "text/javascript" },
+  app: {
+    head: {
+      title: "WSの小屋",
+      meta: [
+        { name: "author", content: "wsvaio@qq.com" },
+        {
+          name: "description",
+          content: "你就像天外来物一样求之不得！",
+        },
+      ],
+      link: [
+        {
+          rel: "stylesheet",
+          type: "text/css",
+          href: "https://cdnjs.cloudflare.com/ajax/libs/aplayer/1.10.1/APlayer.min.css",
+        },
+      ],
+      script: [
+        // { src: "https://fastly.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/autoload.js", type: "text/javascript" },
+        { src: "https://apii.ctose.cn/live2d/Source-One/default/autoload.js", type: "text/javascript" },
+        { src: "https://cdnjs.cloudflare.com/ajax/libs/aplayer/1.10.1/APlayer.min.js", type: "text/javascript" },
 
-			],
-		},
+      ],
+    },
 
-		// PageTransitionEvent: { name: "page", mode: "out-in" },
+    // PageTransitionEvent: { name: "page", mode: "out-in" },
 
-		// layoutTransition: {
-		// 	name: "page",
-		// 	mode: "out-in" // 默认值
-		// },
-		// pageTransition: {
-		// 	name: "page",
-		// 	mode: "out-in" // 默认值
-		// },
+    // layoutTransition: {
+    // 	name: "page",
+    // 	mode: "out-in" // 默认值
+    // },
+    // pageTransition: {
+    // 	name: "page",
+    // 	mode: "out-in" // 默认值
+    // },
 
-	},
+  },
 
-	css: ["~/assets/css/main.less"],
+  css: ["~/assets/css/main.less"],
 
-	modules: ["@unocss/nuxt", "@vueuse/nuxt", "@pinia/nuxt"],
+  modules: ["@unocss/nuxt", "@vueuse/nuxt", "@pinia/nuxt"],
   // modules: ["@nuxtjs/stylelint-module", "@nuxtjs/eslint-module", "@unocss/nuxt", "@vueuse/nuxt", "@pinia/nuxt"],
 
-	nitro: {
-		imports: {
-			dirs: ["~/server/utils"],
-		},
-	},
+  nitro: {
+    imports: {
+      dirs: ["~/server/utils"],
+    },
+  },
 
-	// experimental: {
-	// 	viewTransition: true,
+  // experimental: {
+  // 	viewTransition: true,
 
-	// }
+  // }
 });
--- uno.config.ts
+++ uno.config.ts
@@ -1,30 +1,30 @@
 // uno.config.ts
 import {
-	defineConfig,
-	presetAttributify,
-	presetIcons,
-	presetUno,
-	presetWebFonts,
-	transformerDirectives,
-	transformerVariantGroup
+  defineConfig,
+  presetAttributify,
+  presetIcons,
+  presetUno,
+  presetWebFonts,
+  transformerDirectives,
+  transformerVariantGroup
 } from "unocss";
 
 export default defineConfig({
-	presets: [presetUno({ dark: "class" }), presetAttributify(), presetIcons(), presetWebFonts({
-		provider: "bunny", // 默认提供者
-		fonts: {
-			// 这些将扩展默认主题
+  presets: [presetUno({ dark: "class" }), presetAttributify(), presetIcons(), presetWebFonts({
+    provider: "bunny", // 默认提供者
+    fonts: {
+      // 这些将扩展默认主题
 
-			// 自定义的
-			sans: "ZCOOL XiaoWei",
+      // 自定义的
+      sans: "ZCOOL XiaoWei",
 
-		}
+    }
 
-	})],
-	transformers: [
-		transformerDirectives({
-			enforce: "pre",
-		}),
-		transformerVariantGroup(),
-	],
+  })],
+  transformers: [
+    transformerDirectives({
+      enforce: "pre",
+    }),
+    transformerVariantGroup(),
+  ],
 });
