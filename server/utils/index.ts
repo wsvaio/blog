27459e50a52d8707147cb7578e7734cf24e5c2f5
wsvaio <wsvaio@qq.com>
2024-03-01 14:05:51 +0800

    build: update

--- server/utils/index.ts
+++ server/utils/index.ts
@@ -3,28 +3,24 @@ import nodemailer from "nodemailer";
 
 export const db = new PrismaClient();
 export const transporter = nodemailer.createTransport({
-	service: "qq",
-	secure: true,
-	auth: {
-		user: "wsvaio@qq.com",
-		pass: import.meta.env.EMAIL_PASS,
-	},
+  service: "qq",
+  secure: true,
+  auth: {
+    user: "wsvaio@qq.com",
+    pass: import.meta.env.EMAIL_PASS,
+  },
 
 });
 
-export const arrayToTree = <T extends object, C extends string = "children">(
-	array: T[],
-	id?: string | number | symbol | null,
-	options = {} as { idKey?: string; pidKey?: string; childrenKey?: C }
-): T[] | undefined => {
-	const { idKey = "id", pidKey = "pid", childrenKey = "children" } = options;
+export function arrayToTree<T extends object, C extends string = "children">(array: T[],	id?: string | number | symbol | null,	options = {} as { idKey?: string; pidKey?: string; childrenKey?: C }): T[] | undefined {
+  const { idKey = "id", pidKey = "pid", childrenKey = "children" } = options;
 	type Trees = (T & { [K in C]: Trees })[];
 	const trees: Trees = [];
 	for (const item of array) {
-		// @ts-expect-error pass
-		if (item[pidKey] == id)
-			// @ts-expect-error pass
-			trees.push({ ...item, [childrenKey]: arrayToTree(array, item[idKey], { idKey, pidKey, childrenKey }) });
+	  // @ts-expect-error pass
+	  if (item[pidKey] == id)
+	  // @ts-expect-error pass
+	    trees.push({ ...item, [childrenKey]: arrayToTree(array, item[idKey], { idKey, pidKey, childrenKey }) });
 	}
 	return trees;
-};
+}
