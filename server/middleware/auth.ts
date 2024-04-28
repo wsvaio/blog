f3b01d8cb0641fc9f91b28f7b5a8e953be7ac245
wsvaio <wsvaio@qq.com>
2024-04-28 15:39:14 +0800

    fix: 评论无法回复

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,22 +1,29 @@
 import jwt from "jsonwebtoken";
 
 export default defineEventHandler(async event => {
-  if (
-    (!["get", "options"].includes(event.method.toLowerCase()) && !event.path.startsWith("/api/admin/login")) && !event.path.startsWith("/api/file") && !(/\/api\/article\/\d+\/comment/.test(event.path))
-    || event.path.startsWith("/api/admin/info")
+  if (["get", "options"].includes(event.method.toLowerCase()))
+    return;
+  if (event.path.startsWith("/api/admin/login"))
+    return;
+  if (event.path.startsWith("/api/file"))
+    return;
+  if (/\/api\/article\/\d+\/comment/.test(event.path))
+    return;
+  if (/\/api\/comment\/\d+\/comment/.test(event.path))
+    return;
+  if (event.path.startsWith("/api/admin/info"))
+    return;
 
-  ) {
-    const authorization = getHeader(event, "Authorization") || "";
+  const authorization = getHeader(event, "Authorization") || "";
 
-    // if (!authorization) return new Error("Authorization不存在");
+  // if (!authorization) return new Error("Authorization不存在");
 
-    try {
-      // 偷懒暂时先拿 EMAIL_PASS 做secretOrPublicKey
-      const admin = jwt.verify(authorization, import.meta.env.EMAIL_PASS);
-      event.context.admin = admin;
-    }
-    catch (err) {
-      throw new Error("无可奈何");
-    }
+  try {
+    // 偷懒暂时先拿 EMAIL_PASS 做secretOrPublicKey
+    const admin = jwt.verify(authorization, import.meta.env.EMAIL_PASS);
+    event.context.admin = admin;
+  }
+  catch (err) {
+    throw new Error("无可奈何");
   }
 });
